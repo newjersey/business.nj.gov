811c8c97a1c7e1f03462978d1fa62e6284a3dc64
Anne LoVerso <anne.loverso@gmail.com>
2021-05-26 12:29:35 -0400

    wip: add vpc

--- api/src/functions/express/index.ts
+++ api/src/functions/express/index.ts
@@ -14,4 +14,8 @@ export default {
       },
     },
   ],
+  vpc: {
+    securityGroupIds: ["sg-032a42ecbcea549fa"],
+    subnetIds: ["subnet-09a6dc189433984dd", "subnet-016f1da4d19a7c3b2"]
+  }
 };
